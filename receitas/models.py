b31173619d03f54c900f212d95f14e55b4812cc9
danilo <danilosantos.itech@gmail.com>
2022-02-03 16:32:35 -0300

    ordenação e edição no admin

--- receitas/models.py
+++ receitas/models.py
@@ -7,7 +7,6 @@ from pessoas.models import Pessoa
 
 class Receita(models.Model):
     pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
-   
     nome_receita =  models.CharField(max_length=200)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
@@ -15,5 +14,6 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
+    publicada = models.BooleanField(default=False)
 
 # Create your models here.

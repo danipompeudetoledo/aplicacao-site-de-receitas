ebed9496b379b3d28ee32df60a24aef5aca7c99b
danilo <danilosantos.itech@gmail.com>
2022-02-21 15:29:49 -0300

    token csrf

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render,redirect
+from django.contrib.auth.models import User
+
 
 def cadastro(request):
-    return render(request,'usuarios/cadastro.html')
+    if request.method  == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['password']
+        senha2 = request.POST['password2'] 
+        if not nome.strip():
+            print('o campo nome não pode ficar em branco')
+            return redirect ('cadastro')
+
+        if not email.strip():
+            print('o campo nome não pode ficar em branco')
+            return redirect ('cadastro')
+
+        if senha != senha2:
+            print('As senhas não são iguais')
+            return redirect('cadastro')
+
+        if User.objects.filter(email=email).exists():
+            print('usuario já cadastrado')
+            return redirect ('cadastro')
+
+        user = User.objects.create_user(username=nome, email=email,password=senha)
+        user.save()
+        print("Usuario cadastrado com sucesso")
+        return redirect('login')
+    else:
+        return render(request,'usuarios/cadastro.html')
 
 def login(request):
     return render(request,'usuarios/login.html')
